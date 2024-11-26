2c24ea4644610b9298000899b4b30aa654874e7f
Benson Bird <benson.bird@students.snow.edu>
2024-11-26 10:23:54 -0700

    RefactoredTests

--- client/src/tests/vehicleUtils.test.ts
+++ client/src/tests/vehicleUtils.test.ts
@@ -4,22 +4,23 @@ import { MovementAction } from "../models/MovementAction";
 import { ColorOption } from "../models/ColorOption";
 import { moveVehicle } from "../vehicleUtils";
 
+const baseVehicle = {
+  id: 0,
+  xPosition: 0,
+  yPosition: 0,
+  angleInDegrees: 0,
+  movementAction: MovementAction.MOVE_FORWARD,
+  color: ColorOption.PURPLE,
+};
+
 test("A user can turn the vehicle left", () => {
   const startingVehicle: PlayerVehicle = {
-    id: 0,
-    xPosition: 0,
-    yPosition: 0,
-    angleInDegrees: 0,
+    ...baseVehicle,
     movementAction: MovementAction.TURNING_LEFT,
-    color: ColorOption.PURPLE,
   };
   const expectedVehicle: PlayerVehicle = {
-    id: 0,
-    xPosition: 0,
-    yPosition: 0,
+    ...baseVehicle,
     angleInDegrees: 45,
-    movementAction: MovementAction.MOVE_FORWARD,
-    color: ColorOption.PURPLE,
   };
   expect(moveVehicle(startingVehicle)).toStrictEqual(expectedVehicle);
 });
